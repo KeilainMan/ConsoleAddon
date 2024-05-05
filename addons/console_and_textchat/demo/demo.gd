c766c43fe5472db7d2c1740cc2b9dbc45f695b6d
KeilainMan <109026727+KeilainMan@users.noreply.github.com>
2024-05-05 21:01:38 +0200

    screenshots

--- addons/console_and_textchat/demo/demo.gd
+++ addons/console_and_textchat/demo/demo.gd
@@ -9,7 +9,7 @@ func _ready() -> void:
 	c_a_t.print_message(c_a_t.col(Color.GREEN, "Hi!"))
 	c_a_t.print_message(c_a_t.bold(c_a_t.col("#984447", "This is bold and red dummy text")))
 	c_a_t.print_message(c_a_t.italic(c_a_t.crossed("This is italic and crossed dummy text")))
-	c_a_t.print_message(c_a_t.col(Color.BLUE_VIOLET, c_a_t.underline("It is already " + c_a_t.timestamp() + " a late time!")))
+	c_a_t.print_message(c_a_t.col(Color.BLUE_VIOLET, c_a_t.underline("It is already " + c_a_t.timestamp() + "! A late time!")))
 	c_a_t.register_command("cheat", cheat, false)
 	c_a_t.register_command("troll", troll)
 
